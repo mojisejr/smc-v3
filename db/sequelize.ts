80d5e30bf64e153e1637f4e9df69c4da733cd765
mojisejr <nonthasak.l@gmail.com>
2025-10-25 23:47:19 +0700

    upd: sqlite problem

--- db/sequelize.ts
+++ db/sequelize.ts
@@ -3,5 +3,5 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: `resources/db/database.db`,
-  logging: true,
+  logging: false,
 });
